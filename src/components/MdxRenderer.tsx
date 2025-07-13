697a003d08a7c02c1ee32c78dd1eb0a4b4d944ed
Himanshu Salunke <himanshu.rcpit@gmail.com>
2025-07-13 10:42:54 +0530

    deleted uneccesarry

--- src/components/MdxRenderer.tsx
+++
@@ -1,7 +0,0 @@
-// Code for NEW file: src/components/MdxRenderer.tsx
-"use client" // MDX rendering often needs to be on the client
-import { MDXRemote } from 'next-mdx-remote/rsc';
-
-export function MdxRenderer({ content }: { content: string }) {
-    return <MDXRemote source={content} />;
-}
--- src/components/home/FeaturedProjectsWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { getFeaturedProjects } from "../../utils/server-utils";
-import FeaturedProjects from "./FeaturedProjects";
-
-export default function FeaturedProjectsWrapper() {
-  const featuredProjects = getFeaturedProjects();
-  
-  return <FeaturedProjects projects={featuredProjects} />;
-} 
