436b496abd293909face17a7bea4063be4683478
Himanshu Salunke <himanshu.rcpit@gmail.com>
2025-08-02 00:30:21 +0530

    back to top arrow

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Button, Icon } from "@once-ui-system/core";
+import { Button } from "@once-ui-system/core";
 import styles from "./BackToTop.module.scss";
 
 export default function BackToTop() {
@@ -47,7 +47,22 @@ export default function BackToTop() {
           size="m"
           aria-label="Back to top"
         >
-          ↑
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.arrowIcon}
+          >
+            <path
+              d="M12 4L12 20M12 4L6 10M12 4L18 10"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </Button>
       )}
     </>
