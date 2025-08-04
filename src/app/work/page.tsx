822870bc77c2f65fe23f0cb135cc2d91e26dd1a3
Himanshu Salunke <himanshu.rcpit@gmail.com>
2025-08-05 00:35:04 +0530

    fix

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -10,11 +10,7 @@ import {
 } from "@once-ui-system/core";
 import { baseURL, person, work } from "../../resources";
 import { getAllProjects } from "../../utils/utils";
-
-import FeaturedProjectCard from "../../components/work/FeaturedProjectCard";
-import ProjectGridCard from "../../components/work/ProjectGridCard";
-
-import styles from "./WorkPage.module.scss";
+import WorkPageClient from "../../components/work/WorkPageClient";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -41,80 +37,9 @@ export default async function Work() {
   // ✅ END: NEW LOGIC FOR FILTERING AND RANKING
 
   return (
-    <Column maxWidth="m" gap="40" paddingY="32">
-      <Schema
-        as="webPage"
-        baseURL={baseURL}
-        path={work.path}
-        title={work.title}
-        description={work.description}
-        image="/images/og/home.jpg"
-        author={{
-          name: person.name,
-          url: `${baseURL}/about`,
-          image: `${baseURL}${person.avatar}`,
-        }}
-      />
-      <RevealFx>
-        <Flex fillWidth horizontal="center">
-          <Column horizontal="center" gap="16" maxWidth="s">
-            <Heading as="h1" variant="display-strong-l" align="center">
-              My Work & Projects
-            </Heading>
-            <Text onBackground="neutral-weak" align="center">
-              Here are some of the projects I've worked on. Each one represents
-              a challenge I was excited to solve.
-            </Text>
-          </Column>
-        </Flex>
-      </RevealFx>
-      
-      {featuredProjects.length > 0 && (
-        <Column gap="40">
-          <RevealFx>
-            <Flex vertical="center" gap="20" fillWidth>
-                <Line fillWidth />
-                <Heading as="h2" variant="heading-strong-m" wrap="nowrap">
-                    Featured Projects
-                </Heading>
-                <Line fillWidth />
-            </Flex>
-          </RevealFx>
-          {featuredProjects.map((project, index) => (
-          <div key={project.slug} id={`project-${project.slug}`}>
-            <FeaturedProjectCard
-                project={project}
-                index={index}
-            />
-          </div>
-          ))}
-        </Column>
-      )}
-
-      {otherProjects.length > 0 && (
-        <Column gap="40" paddingY="32">
-          <RevealFx>
-            <Flex vertical="center" gap="20" fillWidth>
-              <Line fillWidth />
-              <Heading as="h2" variant="heading-strong-m" wrap="nowrap">
-                Other Projects
-              </Heading>
-              <Line fillWidth />
-            </Flex>
-          </RevealFx>
-
-          <Flex wrap gap="24" className={styles.gridContainer}>
-            {otherProjects.map((project, index) => (
-              <div key={project.slug} id={`project-${project.slug}`} className={styles.gridItem}>
-                <ProjectGridCard
-                  project={project}
-                  index={index}
-                />
-              </div>
-            ))}
-          </Flex>
-        </Column>
-      )}
-    </Column>
+    <WorkPageClient 
+      featuredProjects={featuredProjects}
+      otherProjects={otherProjects}
+    />
   );
 }
---
+++ src/components/work/WorkPageClient.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import {
+  Column,
+  Schema,
+  Heading,
+  Text,
+  RevealFx,
+  Flex,
+  Line,
+} from "@once-ui-system/core";
+import { baseURL, person, work } from "../../resources";
+import type { MdxContent } from "../../utils/utils";
+
+import FeaturedProjectCard from "./FeaturedProjectCard";
+import ProjectGridCard from "./ProjectGridCard";
+
+import styles from "../../app/work/WorkPage.module.scss";
+
+interface WorkPageClientProps {
+  featuredProjects: MdxContent[];
+  otherProjects: MdxContent[];
+}
+
+export default function WorkPageClient({ 
+  featuredProjects, 
+  otherProjects 
+}: WorkPageClientProps) {
+  return (
+    <Column maxWidth="m" gap="40" paddingY="32">
+      <Schema
+        as="webPage"
+        baseURL={baseURL}
+        path={work.path}
+        title={work.title}
+        description={work.description}
+        image="/images/og/home.jpg"
+        author={{
+          name: person.name,
+          url: `${baseURL}/about`,
+          image: `${baseURL}${person.avatar}`,
+        }}
+      />
+      <RevealFx>
+        <Flex fillWidth horizontal="center">
+          <Column horizontal="center" gap="16" maxWidth="s">
+            <Heading as="h1" variant="display-strong-l" align="center">
+              My Work & Projects
+            </Heading>
+            <Text onBackground="neutral-weak" align="center">
+              Here are some of the projects I've worked on. Each one represents
+              a challenge I was excited to solve.
+            </Text>
+          </Column>
+        </Flex>
+      </RevealFx>
+      
+      {featuredProjects.length > 0 && (
+        <Column gap="40">
+          <RevealFx>
+            <Flex vertical="center" gap="20" fillWidth>
+                <Line fillWidth />
+                <Heading as="h2" variant="heading-strong-m" wrap="nowrap">
+                    Featured Projects
+                </Heading>
+                <Line fillWidth />
+            </Flex>
+          </RevealFx>
+          {featuredProjects.map((project, index) => (
+          <div key={project.slug} id={`project-${project.slug}`}>
+            <FeaturedProjectCard
+                project={project}
+                index={index}
+            />
+          </div>
+          ))}
+        </Column>
+      )}
+
+      {otherProjects.length > 0 && (
+        <Column gap="40" paddingY="32">
+          <RevealFx>
+            <Flex vertical="center" gap="20" fillWidth>
+              <Line fillWidth />
+              <Heading as="h2" variant="heading-strong-m" wrap="nowrap">
+                Other Projects
+              </Heading>
+              <Line fillWidth />
+            </Flex>
+          </RevealFx>
+
+          <Flex wrap gap="24" className={styles.gridContainer}>
+            {otherProjects.map((project, index) => (
+              <div key={project.slug} id={`project-${project.slug}`} className={styles.gridItem}>
+                <ProjectGridCard
+                  project={project}
+                  index={index}
+                />
+              </div>
+            ))}
+          </Flex>
+        </Column>
+      )}
+    </Column>
+  );
+} 
